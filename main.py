01bd431d9d61a37e5ddf9c1e2777baa35ddf20fa
Simon <Simon>
2025-07-17 21:01:57 +0100

    Domain sample inputs and outputs included

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from simulation.run_sim import run_sim
     
 if __name__ == '__main__':
 
-    in_file_names = ['domain_template']
+    in_file_names = ['domain_sample_double_orbit', 'domain_sample_multiple_objects', 'domain_sample_bounce']
 
     run_sim(in_file_names)
